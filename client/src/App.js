2b4b022a813e1e51eb25c923eb8d049fe2e5646b
MegMathis <mcdonough.megan25@gmail.com>
2023-05-10 14:42:08 -0400

    day_2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <></>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios.get("/auth/authenticated").then((res) => {
+      console.log((res) => {
+        setUser(res.data.user);
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        {user && <Route path="/dashboard" element={<Dashboard />} />}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
